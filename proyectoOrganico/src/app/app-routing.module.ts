1e9860f7e77460c0bb5c90f448869c3093cf00d3
Sofia Maccarone <sofiamaccarone@gmail.com>
2021-08-23 16:47:37 -0300

    router-outlet y cambios en nosotros y productos

--- proyectoOrganico/src/app/app-routing.module.ts
+++ proyectoOrganico/src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarritoComponent } from './carrito/carrito.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
+import { ProductosComponent } from './productos/productos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'productos',
+    component: ProductosComponent
+  },
+  {
+    path: 'contacto',
+    component: ContactoComponent
+  },
+  {
+    path: 'nosotros',
+    component: NosotrosComponent
+  },
+  {
+    path: 'carrito',
+    component: CarritoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
