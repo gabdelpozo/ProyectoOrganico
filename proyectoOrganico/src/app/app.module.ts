81b061a5a235c3383ff9f5e734738a636472ea8c
Sofia Maccarone <sofiamaccarone@gmail.com>
2021-08-23 13:22:04 -0300

    agrego componentes

--- proyectoOrganico/src/app/app.module.ts
+++ proyectoOrganico/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductosComponent } from './productos/productos.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { CarritoComponent } from './carrito/carrito.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductosComponent,
+    NosotrosComponent,
+    ContactoComponent,
+    CarritoComponent
   ],
   imports: [
     BrowserModule,
